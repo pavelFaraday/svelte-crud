c12fa021a9689242472f0bb02d1ce6df8496ef70
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-07-31 22:43:08 +0400

    add Bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -13,35 +13,55 @@
 </script>
 
 <main>
-  <form on:submit|preventDefault={onSubmitHandler}>
-    <input
-      bind:value={product.name}
-      type="text"
-      placeholder="Product Name"
-      name
-      id="product-name"
-    />
-    <textarea
-      bind:value={product.decription}
-      id="product-description"
-      placeholder="Product Description"
-      rows="3"
-    ></textarea>
-    <input
-      bind:value={product.imageURL}
-      type="url"
-      id="product-image-url"
-      placeholder="https://faztweb.com/"
-    />
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <!-- First Column -->
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <form on:submit|preventDefault={onSubmitHandler}>
+              <input
+                bind:value={product.name}
+                type="text"
+                placeholder="Product Name"
+                name
+                id="product-name"
+                class="form-control"
+              />
+              <textarea
+                bind:value={product.decription}
+                id="product-description"
+                placeholder="Product Description"
+                rows="3"
+                class="form-control"
+              ></textarea>
+              <input
+                bind:value={product.imageURL}
+                type="url"
+                id="product-image-url"
+                placeholder="https://faztweb.com/"
+                class="form-control"
+              />
 
-    <select bind:value={product.category} id="category">
-      <option value="laptops">Laptops</option>
-      <option value="peripherials">Peripherials</option>
-      <option value="servers">Servers</option>
-    </select>
+              <select
+                bind:value={product.category}
+                id="category"
+                class="form-control"
+              >
+                <option value="laptops">Laptops</option>
+                <option value="peripherials">Peripherials</option>
+                <option value="servers">Servers</option>
+              </select>
 
-    <button>Save Product</button>
-  </form>
+              <button>Save Product</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
