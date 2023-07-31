67f9571189b6f08305b962b9e84476cb4b041271
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-07-31 22:32:41 +0400

    create form | Svelte Bind: | Svelte preventDefault

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,48 @@
 <script>
-	
+  let product = {
+    id: "",
+    name: "",
+    decription: "",
+    category: "",
+    imageURL: "",
+  };
+
+  const onSubmitHandler = (e) => {
+    console.log(product);
+  };
 </script>
 
 <main>
-	<h1>Hello World</h1>
+  <form on:submit|preventDefault={onSubmitHandler}>
+    <input
+      bind:value={product.name}
+      type="text"
+      placeholder="Product Name"
+      name
+      id="product-name"
+    />
+    <textarea
+      bind:value={product.decription}
+      id="product-description"
+      placeholder="Product Description"
+      rows="3"
+    ></textarea>
+    <input
+      bind:value={product.imageURL}
+      type="url"
+      id="product-image-url"
+      placeholder="https://faztweb.com/"
+    />
+
+    <select bind:value={product.category} id="category">
+      <option value="laptops">Laptops</option>
+      <option value="peripherials">Peripherials</option>
+      <option value="servers">Servers</option>
+    </select>
+
+    <button>Save Product</button>
+  </form>
 </main>
 
 <style>
-
 </style>
